6e8965ac8ebd4aadfa9c5e8de04ebe8e987fc14f
rehmanghani2 <rehmanghani2323@gmail.com>
2024-12-09 22:34:50 +0500

    Lab8

---
+++ Lab8/MVCInPython/PythonFramework/DjangoMVC/.venv/Scripts/bookstore/books/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
